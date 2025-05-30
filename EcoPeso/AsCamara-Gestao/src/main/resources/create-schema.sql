9dbaa696917cdc8a26c398a887e82260de808002
ThaynaraxX <silvathaynara428@gmail.com>
2025-05-30 15:05:34 -0300

    Primeiro commit do projeto EcoPeso

---
+++ EcoPeso/AsCamara-Gestao/src/main/resources/create-schema.sql
@@ -0,0 +1,7 @@
+CREATE SCHEMA IF NOT EXISTS reciclagem_schema;
+
+-- usuários de teste
+INSERT INTO usuario (nome, sobrenome, email, username, senha, role, data_criacao, data_atualizacao)
+VALUES ('Crispim', 'Luiz', 'crispim@gmail.com', 'crispimluiz', '123456', 'ROLE_USER', '2025-05-19 15:00:00', '2025-05-19 15:00:00');
+
+
